dc57f393d38b33f9d3fb7bad0763232989e754e8
Arjun <arjun_p_uk@yahoo.co.uk>
2025-06-14 13:15:58 +0100

    tokeniser done

--- tokeniser.py
+++ tokeniser.py
@@ -32,7 +32,7 @@ vocab.sort()
 eod = "</w>"
 vocab.append(eod)
 
-print(len(vocab))
+#print(len(vocab))
 
 #using a dictionary that acts as a frequency table for the words
 word_splits = {}
@@ -47,7 +47,7 @@ for doc in corpus:
                 word_splits[word_tuple] = 0
             word_splits[word_tuple] += 1
 
-print(word_splits) #{('T', 'h', 'i', 's', '</w>'): 2,...}
+#print(word_splits) #{('T', 'h', 'i', 's', '</w>'): 2,...}
 
 ###iterative merging
 # take each word split and count the frequency of each adjacent pair of symbols across corpus
@@ -83,7 +83,7 @@ def merge_pair(pair_to_merge, splits):
             and symbols[i] == first \
             and symbols[i+1] == second:
                 new_symbols.append(merged_token)
-
+                i += 2
             else:
                 new_symbols.append(symbols[i])
                 i += 1
@@ -91,3 +91,40 @@ def merge_pair(pair_to_merge, splits):
         new_splits[tuple(new_symbols)] = freq
     
     return new_splits
+
+###iterative byte pair encoding merge loop
+#calculate pair frequencies using get_pair_stats(), then choose top k pairs to inspect and then apply merge_pairs
+num_merges = 15
+merges = {} #store merge rules in here
+current_splits = word_splits.copy()
+
+for i in range(num_merges):
+    print(f"iteration: {i+1}/{num_merges}")
+
+    #calculate pair frequencies
+    pair_stats = get_pair_stats(current_splits)
+    if not pair_stats:
+        print("no more pairs to merge")
+        break
+    
+    sorted_pairs = sorted(pair_stats.items(), key= lambda x: x[1], reverse=True)
+    print(f"top 5: {sorted_pairs[:5]}")
+    #find best pair
+    best_pair = max(pair_stats, key=pair_stats.get)
+    best_freq = pair_stats[best_pair]
+    print(f"best pair: {best_pair}, best freq: {best_freq}")
+
+    #merge best pair across all word representations
+    current_splits = merge_pair(best_pair, current_splits)
+    new_token = best_pair[0] + best_pair[1]
+    print(f"new splits: {current_splits}")
+    
+    #add new token to vocab, and add new merge rule to list
+    vocab.append(new_token)
+    merges[best_pair] = new_token
+
+print("final results")
+print(f"Final vocab size: {len(vocab)}")
+print(f"merges learnt: {merges}")
+# Final vocab size: 35
+# merges learnt: {('s', '</w>'): 's</w>', ('i', 's</w>'): 'is</w>', ('t', 'h'): 'th', ('th', 'e'): 'the', ('the', '</w>'): 'the</w>', ('d', 'o'): 'do', ('do', 'c'): 'doc', ('doc', 'u'): 'docu', ('docu', 'm'): 'docum', ('docum', 'e'): 'docume', ('docume', 'n'): 'documen', ('documen', 't'): 'document', ('i', 'r'): 'ir', ('.', '</w>'): '.</w>', ('d', '</w>'): 'd</w>'}      
