2fdbaf85a1b71405c572227bab564a69dde81f6d
Arjun <arjun_p_uk@yahoo.co.uk>
2025-06-15 10:18:41 +0100

    more tokeniser planning

--- tokeniser/tokeniser.py
+++ tokeniser/tokeniser.py
@@ -1,1 +1,3 @@
-#create tokeniser class, which will 
+#create tokeniser class, which will take in a corpus (list of documents) and output a vocab json file.
+#maybe it should also take in a merge rule parameter, to modify how it merges.
+#also need to add code to detokenise (take token id and turn it into text)
