e19c67c40fc632c7af6a14579cda0c6333ed007d
Arjun <arjun_p_uk@yahoo.co.uk>
2025-06-16 18:19:58 +0100

    tokeniser pretty much done

--- tokeniser/tokeniser.py
+++ tokeniser/tokeniser.py
@@ -5,55 +5,62 @@ import collections
 
 class Tokeniser:
     def __init__(self, merges=200):
-        self.vocab = vocab
+        self.vocab = {}
+        self.inverse_vocab = {}
         self.merges = merges
-        #####################################refactor below
-        #initialise_vocab()
-        #initialise_word_splits()
-        #update_vocab()
-        #load_vocab_to_json()
+        self.merge_list = {}
+        self.eod = "</w>"
+        
+    def train(self, corpus):
+        self._initialise_vocab(corpus)
+        current_splits = self._initialise_word_splits(corpus)
+        self._update_vocab(current_splits)
+        print("final results")
+        print(f"Final vocab size: {len(self.vocab)}")
+        print(f"merges learnt: {self.merge_list}")
+        print(self.vocab)
+        self._load_vocab_to_json()
 
+
+    def encode(self, text):
+        pass
+
+    def decode(self, tokens):
+        pass
+    
+    def _load_vocab_to_json(self):
+        pass
+
+    def _initialise_vocab(self, corpus):
         unique_chars = set()
         for doc in corpus:
             for char in doc:
                 unique_chars.add(char)
-
-        #convert to list so it's mutable
         vocab = list(unique_chars)
         vocab.sort()
+        vocab.append(self.eod)
+        self.vocab = vocab
 
-        ### pre tokenisation
-        # need to add special end of word token to mark word boundaries.
-
-        eod = "</w>"
-        vocab.append(eod)
-
-        #print(len(vocab))
-
-        #using a dictionary that acts as a frequency table for the words
+    def _initialise_word_splits(self, corpus):
         word_splits = {}
         for doc in corpus:
             words = doc.split(' ')
             for word in words:
                 if word:
-                    char_list = list(word) + [eod]
-                    #using tuple for immutability, so it can be used as a dictionary key
+                    char_list = list(word) + [self.eod]
                     word_tuple = tuple(char_list)
                     if word_tuple not in word_splits:
                         word_splits[word_tuple] = 0
                     word_splits[word_tuple] += 1
 
-                ###iterative byte pair encoding merge loop
-        #calculate pair frequencies using get_pair_stats(), then choose top k pairs to inspect and then apply merge_pairs
-        num_merges = 15
-        merges = {} #store merge rules in here
-        current_splits = word_splits.copy()
+        return word_splits
 
-        for i in range(num_merges):
-            print(f"iteration: {i+1}/{num_merges}")
+    def _update_vocab(self, current_splits):
+        for i in range(self.merges):
+            print(f"iteration: {i+1}/{self.merges}")
 
             #calculate pair frequencies
-            pair_stats = get_pair_stats(current_splits)
+            pair_stats = self._get_pair_stats(current_splits)
             if not pair_stats:
                 print("no more pairs to merge")
                 break
@@ -66,29 +73,15 @@ class Tokeniser:
             print(f"best pair: {best_pair}, best freq: {best_freq}")
 
             #merge best pair across all word representations
-            current_splits = merge_pair(best_pair, current_splits)
+            current_splits = self._merge_pair(best_pair, current_splits)
             new_token = best_pair[0] + best_pair[1]
             print(f"new splits: {current_splits}")
             
             #add new token to vocab, and add new merge rule to list
-            vocab.append(new_token)
-            merges[best_pair] = new_token
+            self.vocab.append(new_token)
+            self.merge_list[best_pair] = new_token
 
-        print("final results")
-        print(f"Final vocab size: {len(vocab)}")
-        print(f"merges learnt: {merges}")
-        print(vocab)
-    #########################################refactor above
-    def train(self, corpus):
-        pass
-
-    def encode(self, text):
-        pass
-
-    def decode(self, tokens):
-        pass
-    
-    def get_pair_stats(splits):
+    def _get_pair_stats(self, splits):
         pair_counts = collections.defaultdict(int)
         
         for word_tuple, freq in splits.items():
@@ -100,11 +93,7 @@ class Tokeniser:
 
         return pair_counts
 
-#print(get_pair_stats(word_splits)) #{('T', 'h'): 2, ('h', 'i'): 5, ...}
-
-#merge the common pairs, e.g. "is" is common, so merge "i" and "s"
-
-    def merge_pair(pair_to_merge, splits):
+    def _merge_pair(self, pair_to_merge, splits):
         new_splits = {}
         (first, second) = pair_to_merge
         merged_token = first + second
---
+++ train.py
@@ -0,0 +1,11 @@
+from tokeniser.tokeniser import Tokeniser
+
+corpus = [
+    "This is the first document.",
+    "This document is the second document.",
+    "And this is the third one.",
+    "Is this the first document?"
+]
+
+tokeniser = Tokeniser(merges=15)
+tokeniser.train(corpus)
